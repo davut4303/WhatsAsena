c3eed6be70e50cae64fb5b80003110158140bad6
Davut <80701933+davut4303@users.noreply.github.com>
2022-02-05 19:06:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,8 @@
-/* Copyright (C) 2020 Yusuf Usta.
+/* Copyright (C) 2020 
 
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
 
-WhatsAsena - Yusuf Usta
+
+WhatsAsena - gbariel
 */
 
 const fs = require("fs");
